f42a8d2bf68daac0c6d865548e4bb6f01eff0237
bwynalek <58231608+bwynalek@users.noreply.github.com>
2020-03-22 20:17:18 -0600

    Added Main Menu Scene, Art, and Scripts
    -Added MainMenu scene
    -Added MainMenu folder
    -Added Art and Scripts folders within MainMenu folder
    -Added images for MainMenu
    -Added MainMenuController script

---
+++ Event Horizon/Assets/Scenes/MainMenu/Scripts/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject ControlsMenu;
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void options()
+    {
+        MainMenu.SetActive(false);
+        ControlsMenu.SetActive(true);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
