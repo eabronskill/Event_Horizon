497f37cfdd47faf0b314bbc27a9dc22df9e01095
eabronskill <eabronskill2017@gmail.com>
2020-04-21 14:07:26 -0700

    automatic doors

--- Event Horizon/Assets/Scripts/Triggers/Doors/DoorOpen.cs
+++ Event Horizon/Assets/Scripts/Triggers/Doors/DoorOpen.cs
@@ -23,38 +23,39 @@ public class DoorOpen : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            if (col.gameObject.name.Equals("Tank Controller"))
-            {
-                player = col.GetComponent<TankInput>().player;
-                if (player.GetButtonDown("Interact"))
-                {
-                    open = true;
-                }
-            }
-            if (col.gameObject.name.Equals("Soldier Controller"))
-            {
-                player = col.GetComponent<SoldierInput>().player;
-                if (player.GetButtonDown("Interact"))
-                {
-                    open = true;
-                }
-            }
-            if (col.gameObject.name.Equals("Engineer Controller"))
-            {
-                player = col.GetComponent<TechnicianInput>().player;
-                if (player.GetButtonDown("Interact"))
-                {
-                    open = true;
-                }
-            }
-            if (col.gameObject.name.Equals("Rogue Controller"))
-            {
-                player = col.GetComponent<rogueInput>().player;
-                if (player.GetButtonDown("Interact"))
-                {
-                    open = true;
-                }
-            }
+            //if (col.gameObject.name.Equals("Tank Controller"))
+            //{
+            //    player = col.GetComponent<TankInput>().player;
+            //    if (player.GetButtonDown("Interact"))
+            //    {
+            //        open = true;
+            //    }
+            //}
+            //if (col.gameObject.name.Equals("Soldier Controller"))
+            //{
+            //    player = col.GetComponent<SoldierInput>().player;
+            //    if (player.GetButtonDown("Interact"))
+            //    {
+            //        open = true;
+            //    }
+            //}
+            //if (col.gameObject.name.Equals("Engineer Controller"))
+            //{
+            //    player = col.GetComponent<TechnicianInput>().player;
+            //    if (player.GetButtonDown("Interact"))
+            //    {
+            //        open = true;
+            //    }
+            //}
+            //if (col.gameObject.name.Equals("Rogue Controller"))
+            //{
+            //    player = col.GetComponent<rogueInput>().player;
+            //    if (player.GetButtonDown("Interact"))
+            //    {
+            //        open = true;
+            //    }
+            //}
+            open = true;
         }
     }
 
